944e6d3fd22082f28f77207d5ebbd42723ed0507
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-07-15 07:05:59 +0200

    WIP :: Update escenaries files

---
+++ database/migrations/2022_07_15_050259_create_sceneries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSceneriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sceneries', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->text('file_path');
+            $table->text('bit_map')->nullable();
+            $table->integer('parameter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sceneries');
+    }
+}
